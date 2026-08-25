52dbc1320c54a227c123707f22225bce2014d6ce
Andrew A Lee <andrew@andrewleedev.com>
2026-08-24 19:14:25 -0700

    Adds video thumbnail image file handling

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,9 +1,9 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 
 type Thumbnail = {
   data: ArrayBuffer;
@@ -45,9 +45,44 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const token = getBearerToken(req.headers);
   const userID = validateJWT(token, cfg.jwtSecret);
 
-  console.log("uploading thumbnail for video", videoId, "by user", userID);
+  const video = getVideo(cfg.db, videoId);
+  if (!video) {
+    throw new NotFoundError("Video resource not found");
+  }
 
-  // TODO: implement the upload here
+  if (video.userID !== userID) {
+    throw new UserForbiddenError("Not authorized to modify resource");
+  }
 
-  return respondWithJSON(200, null);
+  const formData = await req.formData();
+  const file = formData.get("thumbnail");
+
+  if (!(file instanceof File)) {
+    throw new BadRequestError("Thumbnail file missing");
+  }
+
+  const MAX_UPLOAD_SIZE = 10 << 20;
+
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("File exceeds 10MB size limit");
+  }
+
+  const mediaType = file.type;
+  if (!mediaType) {
+    throw new BadRequestError("Missing Content-Type for thumbnail");
+  }
+
+  const data = await file.arrayBuffer();
+  if (!data) {
+    throw new Error("Error reading file data");
+  }
+
+  videoThumbnails.set(videoId, { data, mediaType });
+
+  const thumbnailURL = `http://localhost:${cfg.port}/api/thumbnails/${video.id}`;
+  video.thumbnailURL = thumbnailURL;
+
+  updateVideo(cfg.db, video);
+
+  return respondWithJSON(200, video);
 }
