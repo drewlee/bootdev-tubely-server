392ac9c68a86339c3dcbd2df9ea735631832a478
Andrew A Lee <andrew@andrewleedev.com>
2026-09-08 20:04:47 -0700

    Start work on video upload handler

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -1,8 +1,40 @@
-import { respondWithJSON } from "./json";
-
-import { type ApiConfig } from "../config";
 import type { BunRequest } from "bun";
+import { respondWithJSON } from "./json";
+import { type ApiConfig } from "../config";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import { getBearerToken, validateJWT } from "../auth";
+import { getVideo } from "../db/videos";
 
 export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
+  const MAX_UPLOAD_SIZE = 10 << 30;
+
+  const { videoId } = req.params as { videoId?: string };
+  if (!videoId) {
+    throw new BadRequestError("Invalid video ID");
+  }
+
+  const token = getBearerToken(req.headers);
+  const userID = validateJWT(token, cfg.jwtSecret);
+
+  const video = getVideo(cfg.db, videoId);
+  if (!video) {
+    throw new NotFoundError("Video resource not found");
+  }
+
+  if (video.userID !== userID) {
+    throw new UserForbiddenError("Not authorized to modify resource");
+  }
+
+  const formData = await req.formData();
+  const file = formData.get("video");
+
+  if (!(file instanceof File)) {
+    throw new BadRequestError("Video file missing");
+  }
+
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("File exceeds 1 GB size limit");
+  }
+
   return respondWithJSON(200, null);
 }
